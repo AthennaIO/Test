57544db60fbe3a5876f19e8fa22febf26f7b6262
João Lenon <lenonSec7@gmail.com>
2022-07-07 11:21:52 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * @athenna/template
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export class Bootstrap {
+  /**
+   * Executes the application.
+   *
+   * @param {string[]} args
+   * @return {string}
+   */
+  static main(...args) {
+    return `Bootstrap: ${args.join(' ')}`
+  }
+}
